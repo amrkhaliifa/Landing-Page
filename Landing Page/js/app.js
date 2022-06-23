1d960a66991ae6828ce1f8aca10b78d7d8be4ed9
Amr Khalifa <90288559+amrkhaliifa@users.noreply.github.com>
2022-06-23 23:47:33 +0200

    Add files via upload

---
+++ Landing Page/js/app.js
@@ -0,0 +1,89 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+ */
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+ */
+
+/**
+ * Define Global Variables
+ * 
+ */
+const allSection = document.querySelectorAll("section");
+const navigationBar = document.getElementById("navbar__list");
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+ */
+// function to create Nav Bur 
+function createListNav() {
+    for (const section of allSection) {
+        sName = section.getAttribute("data-nav");
+        sId = section.getAttribute("id");
+        // create list 
+        let createList = document.createElement("li");
+        createList.innerHTML = `<a href="#${sId}" class="menu__link" >${sName}</a>`;
+        navigationBar.appendChild(createList);
+
+    }
+}
+
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+ */
+
+// build the nav
+
+createListNav();
+
+// Add class 'active' to section when near top of viewport
+window.onscroll = function() {
+    document.querySelectorAll("section").forEach(function(active) {
+        if (
+            // Set sections as active
+            active.getBoundingClientRect().top >= -200 && active.getBoundingClientRect().top <= 200
+        ) {
+            // add clas active
+            active.classList.add("your-active-class");
+        } else {
+            // remove clas active
+            active.classList.remove("your-active-class");
+        }
+
+    });
+}
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+ */
+
+
+
+// Scroll to section on link click
